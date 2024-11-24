90694ccf3a00963016c1a9db631802923404b167
Thiago Brunholi <thiago.brunholi@outlook.com>
2024-11-24 12:36:53 -0300

    Data Annotations
    Adicionando os DataAnnotations para as validações de entrada das nossas propriedades das classes.

--- BlazorShop/Models/Product.cs
+++ BlazorShop/Models/Product.cs
@@ -1,10 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazorShop.Models;
 
 public class Product
 {
+    [Key]
     public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Informe o título")]
+    [MinLength(3, ErrorMessage = "O titúlo deve ter no mínimo 3 caracteres")]
+    [MaxLength(120, ErrorMessage = "O título deve ter no máximo 120 caracteres")]
     public string Title { get; set; } = String.Empty;
-    public string Description { get; set; } = String.Empty;
+    
+    public string? Description { get; set; }
+    
+    [Required(ErrorMessage = "Informe o preço do Produto")]
+    [DataType(DataType.Currency)]
     public decimal Price { get; set; }
 
     public int CategoryId { get; set; }
